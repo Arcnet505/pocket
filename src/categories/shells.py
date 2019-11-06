858179239f0a714d2eedb6de55567a506006234b
root <root@localhost.localdomain>
2019-11-06 22:51:12 +1100

    Began work on socket listener

--- src/categories/shells.py
+++ src/categories/shells.py
@@ -3,6 +3,8 @@ from colorama import Fore, Back, Style
 import subprocess
 import http.server
 import socketserver
+import socket
+import time
 
 
 class Shells(Cmd):
@@ -10,11 +12,51 @@ class Shells(Cmd):
     prompt = "[" + Fore.YELLOW + "shells" + Style.RESET_ALL + "] >>> "
 
     def do_listen(self, line):
-        PORT = line
-        subprocess.call('nc -lvnp {}'.format(PORT))
+        "Start a listener by running 'listen PORT'"
+        PORT = int(line)
 
-    def do_pythonws(self, line):
-        "Start a python HTTP web server by running 'pythonws PORT'"
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('localhost', PORT))
+
+        sock.sendall(('GET / HTTP/1.1\nHost: google.com\n\n').encode())
+        time.sleep(0.5)
+        sock.shutdown(socket.SHUT_WR)
+
+        res = ""
+
+        while True:
+            data = sock.recv(1024)
+            if (not data):
+                break
+            res += data.decode()
+
+        print(res)
+
+        print("Connection closed.")
+        sock.close()
+
+        while 1:
+            buf = ""
+            shouldClose = False
+
+            # collect the request
+            inp = input("")
+            while inp != "":
+                # stop processing if we want the conneciton to close
+                if (inp == "Connection: close"):
+                    shouldClose = True
+                buf += inp + "\n"
+                inp = input("")
+
+            buf += "\n"
+
+            # do_listen(port buf.encode())
+
+            if (shouldClose):
+                break
+
+    def do_webserver(self, line):
+        "Start a python HTTP web server by running 'webserver PORT'"
         PORT = int(line)
 
         Handler = http.server.SimpleHTTPRequestHandler
